d3dfadd42ad55f51c0a29c3f2c326a085845e526
Seb97_4 <m_seb@icloud.com>
2024-04-15 17:01:01 +0400

    debug data fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { fetchRevenue } from '@/app/lib/data';
 export default async function Page() {
 
     const revenue = await fetchRevenue();
+    console.log(revenue);
 
     return (
         <main>
